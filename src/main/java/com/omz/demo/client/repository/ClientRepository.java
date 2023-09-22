c1f47736256eaf1f38b005ac75e1ade101080acc
Awesome0619 <gethub0619@gmail.com>
2023-09-22 12:48:23 +0900

    230922

--- src/main/java/com/omz/demo/client/repository/ClientRepository.java
+++ src/main/java/com/omz/demo/client/repository/ClientRepository.java
@@ -13,9 +13,30 @@ public interface ClientRepository extends JpaRepository<ClientEntity, String> {
 
 	public ClientEntity findByClientId(String clientId);
 
+//	JPQL : 엔티티 객체를 조회하는 객체지향 쿼리, 테이블 대상으로 쿼리하는 것이 아닌 엔티티 객체를 대상으로 쿼리
+//	public ClientId findByClientId(@Param("clientId") String clientId)
+//	@Query(value="SELECT c FROM clientEntity WHERE c.clientId=:clientId")
+
+//	nativeQuery : 테이블을 대상으로 쿼리 한다(oracle 기준)
 //	@Query(value = "INSERT INTO omz_client(client_id, client_pass, client_name, phone, email, gender, age, mbti, reg_date, grede)"
-//			+ "VALUES(:#{#client_id}, :#{#entity.client_pass}, :#{#entity.client_name}, :#{#entity.phone}, :#{#entity.email}, :#{#entity.gender}, :#{#entity.age}, :#{#entity.mbti}, sysdate, 0)", nativeQuery = true)
+//	+ "VALUES(:#{#client_id}, :#{#entity.client_pass}, :#{#entity.client_name}, :#{#entity.phone}, :#{#entity.email}, :#{#entity.gender}, :#{#entity.age}, :#{#entity.mbti}, sysdate, 0)", nativeQuery = true)
 //	@Modifying
 //	void saveNew(@Param("entity") ClientEntity entity);
+	
+//	@QueryDSL (JPQL 문법 사용)
+//	EntityManager entityManager = EntityManager.getEntityManager();
+//	JPAQuery queryFactory = new JPAQuery(entityManager);
+//	ClinetEntity result = queryFactory
+//						  .select(clientEntity)
+//						  .from(clinetEntity)
+//						  .where(clientIdEq(clinetId))
+	
+//	ORACLE 방식
+//	FROM board2 b, member2 m
+//	WHERE b.member_email = m.member_email(+)
+//	
+//	ANSI 방식(JPQL)
+//	FROM board2 b left Outer Join member2 m
+//	ON b.member_email = m.member_email
 
 }
